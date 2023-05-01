689c826adb0ff23a96a4989b0d560f433723fdb7
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-30 22:38:01 -0700

    Add exports

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import { apiRouter } from "./api.router.js";
+import { clientRouter } from "./client.router.js";
+import { catchRouter } from "./catch.router.js";
+
+export { apiRouter, clientRouter, catchRouter };
