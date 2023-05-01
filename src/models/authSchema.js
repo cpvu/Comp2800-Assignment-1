08707ec5d55c71f6f37b07d86625a12927008a54
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-30 22:37:03 -0700

    Add mongo schema

---
+++ src/models/authSchema.js
@@ -0,0 +1,12 @@
+import mongoose, { model, Schema } from "mongoose";
+
+export const userModel = model(
+  "users",
+  new Schema(
+    {
+      username: { type: String, required: true },
+      password: { type: String, required: true },
+    },
+    { collection: "users" }
+  )
+);
